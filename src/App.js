f28898a019c8b8ca37c9b62ecd1a8000cdb0edba
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-01-01 23:36:02 +0100

    MarkDown previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {TextArea , PreviewArea }  from './textArea' 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 className="title">MARKDOWN PREVIEWER</h1>
+       <TextArea />
+       <PreviewArea />
     </div>
   );
 }
+
+// create a text area
+// create a preview area that renders the text area
+// add resize buttons
 
 export default App;
---
+++ src/textArea.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css'
+// create a text area
+// create a previeew area that renders the text area
+// add resize buttons
+
+class TextArea extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {}
+    };
+ render () {
+     return (
+        <div className="MarkDiv">
+           <h1 className="markTitle">MarkDown Area</h1>
+           <textarea className="editor"></textarea>
+        </div>
+     )
+  };    
+}
+
+class PreviewArea extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {}
+    };
+ render () {
+     return (
+        <div className="previewDiv">
+           <h1 className="previewTitle">MarkDown Area</h1>
+           <textarea className="previewer"></textarea>
+        </div>
+     )
+  };    
+}
+
+export { TextArea, PreviewArea } 
