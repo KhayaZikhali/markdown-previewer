2854a588cde3ce0315ef86b7c1a5e16c08589436
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-01-03 18:53:29 +0100

    Stremalined the markdown previewer
    Installed a markdown library.
    Removed unnecessary syntax.
    Installed a syntax highlighter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React , {useState}from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
-import {TextArea , PreviewArea }  from './textArea' 
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function App() {
+  const [input, setInput] = useState();
   return (
     <div className="App">
        <h1 className="title">MARKDOWN PREVIEWER</h1>
-       <div className="left">
-         <TextArea/>
-       </div>
-       <div className="right">
-         <PreviewArea />
-       </div>
+       <div>
+        <div className="left">
+        <div className="MarkDiv">
+           <h1 className="markTitle">Input Area</h1>
+           <textarea className="editor" id="editor" value={input} onChange={(e)=>setInput(e.target.value)} ></textarea>
+        </div>
+        </div>
+        <div className="right">
+        <div className="previewDiv">
+           <h1 className="previewTitle">Preview Area</h1>
+           <ReactMarkdown className="previewer" source={input} renderers={{code : Component}}/>
+        </div> 
+        </div>
+       </div> 
     </div>
   );
 }
 
-// create a text area
-// create a preview area that renders the text area
-// add resize buttons
+const Component = ({value, language}) => {
+  return (
+    <SyntaxHighlighter language={language ?? null} style={docco}>
+      {value ?? ''}
+    </SyntaxHighlighter>
+  );
+};
 
 export default App;
